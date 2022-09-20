c74d15946d3a01d195a688da18d4e96fffcf4659
chidinmadominica <chidinmadominica9@gmail.com>
2022-09-19 19:16:05 -0700

    dont hate the hacker hate the code

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -10,3 +13,5 @@ void _putchar(char);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+#endif
