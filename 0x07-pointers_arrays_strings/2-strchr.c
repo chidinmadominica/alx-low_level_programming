fcd4ce6b224604bd5acb66345ac3a96ae098513d
chidinmadominica <chidinmadominica9@gmail.com>
2022-09-27 07:26:33 -0700

    strspn

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,16 +5,23 @@
  *_strchr - locates a character in a string
  * @s: string to search
  * @c: char to find
- * Return: a pointer to the first occurrence of the character c
+ * Return: a pointer to the first occurrence of the character
+ * c in the string s, or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
 {
-	unsigned int i = 0;
+	int a;
 
-	for (; *(s + i) != '\0'; i++)
-		if (*(s + i) == c)
-			return (s + i);
-	if (*(s + i) == c)
-		return ('\0');
-	return (s + i);
-}
+	while (1)
+	{
+		a = *s++;
+		if (a == c)
+		{
+			return (s - 1);
+		}
+		if (a == 0)
+		{
+			return (NULL);
+		}
+						}
+	}
